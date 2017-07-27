a3f3c550d4af6cdc0675c2f3a04c243d4f070d98
Lovita28 <lovitaanjani@gmail.com>
2017-07-27 13:08:52 +0700

    Triangle (operator) task

---
+++ Triangle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Triangle Wide");
+            
+            Console.Write("Please enter the Base :");
+            int b = int.Parse(Console.ReadLine());
+            while (b < 0) {
+                Console.WriteLine("Value of Base cannot negative, Please enter again");
+                Console.WriteLine("");
+                Console.Write("Please enter the Base :");
+                 b = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Please enter the Height :");
+            int h = int.Parse(Console.ReadLine());
+            while (h < 0)
+            {
+                Console.WriteLine("Value of Height cannot negative, Please enter again");
+                Console.WriteLine("");
+                Console.Write("Please enter the Height :");
+                h = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nYou have input Base and Height \n");
+            Console.WriteLine("\nTriangle Wide : {0}", (b * h) / 2);
+
+            Console.WriteLine("Thank you");
+            Console.ReadKey();
+
+        }
+    }
+}
